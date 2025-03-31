b6b4451980a854ba03289c40dc332a616ef90168
caroAM22 <caalvarezmu@unal.edu.co>
2025-03-30 23:14:36 -0500

    Configuración para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/caroAM22.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
